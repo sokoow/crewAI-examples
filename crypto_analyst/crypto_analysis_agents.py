25fdd2fcaab0417c736153b79412647387bded9c
Karol <sokoow@gmail.com>
2024-01-20 18:52:50 +0100

    Removing sec tools import

--- crypto_analyst/crypto_analysis_agents.py
+++ crypto_analyst/crypto_analysis_agents.py
@@ -3,7 +3,6 @@ from crewai import Agent
 from tools.browser_tools import BrowserTools
 from tools.calculator_tools import CalculatorTools
 from tools.search_tools import SearchTools
-from tools.sec_tools import SECTools
 
 from langchain.tools.yahoo_finance_news import YahooFinanceNewsTool
 
